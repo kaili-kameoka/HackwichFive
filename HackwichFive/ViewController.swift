be4a1f9394d3975faae1c3bba387a92473d28593
Kaili Kameoka <kaili.kameoka@gmail.com>
2022-02-21 13:26:09 -1000

    Completed parts 1 - 7

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    var currentIndex = 0;
+    var favoriteFoodsArra = ["Burgers", "Sushi", "Fried Chicken"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topLabel.text =  "My favorite foods:";
+        bottomLabel.text = favoriteFoodsArra[currentIndex];
     }
 
-
+    
 }
 
